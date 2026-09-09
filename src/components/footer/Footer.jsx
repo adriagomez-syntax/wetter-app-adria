888e2edd5fa9418682d95ceff01ec9f60c2b33d6
Adria Gomez <adria.gomez@tn.syntax-institut.de>
2026-09-09 20:39:30 +0200

    Footer + no .env on GitHub

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className="p-4 border-t border-background-dark/20">
+			<p className="text-2xs">© 2024 WetterBlick Telemetrie & Prognose GmbH. Alle Rechte vorbehalten.</p>
+		</footer>
+	)
+}
