66b29d258f8e506352a80d393a45c662990cef83
Adria Gomez <adria.gomez@tn.syntax-institut.de>
2026-09-12 20:44:37 +0200

    Alternative fixing

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
-			"cloudflare:workers": path.resolve(__dirname, 'src/assets/mockEnv.js')
+			"cloudflare:workers": path.resolve('./src/assets/mockEnv.js')
 		}
 	}
 })
